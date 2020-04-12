f468a0ad4b522adc2c63afb80b5eff75c19ed195
Sulay Sumaria <sulay.s@yudiz.in>
2020-04-12 11:09:52 +0530

    Update output logs.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require("path");
 const FILES_TO_IGNORE = [".DS_Store", ".gitkeep"];
 
 function getSizeInMb(bytes) {
-  return (bytes / 1024 / 1024).toFixed(2);
+  return (bytes / 1024 / 1024).toFixed(2).toString().padStart(6, " ");
 }
 
 async function init() {
@@ -24,16 +24,16 @@ async function init() {
 
       jimpImage = await jimpImage.quality(80);
 
-      await jimpImage.write(`output/${image}`);
+      await jimpImage.writeAsync(`output/${image}`);
 
       const compressedStats = fs.statSync(`output/${image}`);
 
       totalBytesSaved += stats.size - compressedStats.size;
 
       console.log(
-        `Before: ${getSizeInMb(stats.size)} MB. After: ${getSizeInMb(
+        `Before: ${getSizeInMb(stats.size)} MB   After: ${getSizeInMb(
           compressedStats.size
-        )} MB. Total Saved: ${getSizeInMb(totalBytesSaved)} MB`
+        )} MB   Total Saved: ${getSizeInMb(totalBytesSaved)} MB`
       );
     } catch (e) {
       console.log(image);
