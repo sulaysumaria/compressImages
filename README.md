18384e60e490f644ca7d2037f6a903ca5631ab44
Sulay Sumaria <sulay.s@yudiz.in>
2020-04-12 10:51:53 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# compressImages
+
+Compress Images with large file size without loosing quality.
+
+## Usage
+
+- Step 0. Run `npm i` to install dependencies.
+- Step 1. Place images to compress in `input` folder.
+- Step 2. Run `npm start`.
+- Step 3. Go and have a coffee while this process completed.
+- Step 4. FInd your compressed files in `output` folder.
+
+## Notes
+
+- Works only with jpg images for now.
---
+++ index.js
@@ -0,0 +1,45 @@
+const Jimp = require("jimp");
+const fs = require("fs");
+const path = require("path");
+
+const FILES_TO_IGNORE = [".DS_Store", ".gitkeep"];
+
+function getSizeInMb(bytes) {
+  return (bytes / 1024 / 1024).toFixed(2);
+}
+
+async function init() {
+  let images = fs.readdirSync(path.join(".", "input"));
+  let totalBytesSaved = 0;
+
+  for (const image of images) {
+    try {
+      if (FILES_TO_IGNORE.includes(image)) {
+        continue;
+      }
+
+      const stats = fs.statSync(`input/${image}`);
+
+      let jimpImage = await Jimp.read(`input/${image}`);
+
+      jimpImage = await jimpImage.quality(80);
+
+      await jimpImage.write(`output/${image}`);
+
+      const compressedStats = fs.statSync(`output/${image}`);
+
+      totalBytesSaved += stats.size - compressedStats.size;
+
+      console.log(
+        `Before: ${getSizeInMb(stats.size)} MB. After: ${getSizeInMb(
+          compressedStats.size
+        )} MB. Total Saved: ${getSizeInMb(totalBytesSaved)} MB`
+      );
+    } catch (e) {
+      console.log(image);
+      console.log(e);
+    }
+  }
+}
+
+init();
